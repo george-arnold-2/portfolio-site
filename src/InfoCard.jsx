aeb12a5bd3163c0d907aa6833cb84a2924797bc3
George Arnold <george@groovecommerce.com>
2025-08-12 10:10:07 -0400

    updates to animation style and buttons

--- src/InfoCard.jsx
+++ src/InfoCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import data from './assets/data.json';
 import './InfoCard.css';
 
@@ -7,12 +7,17 @@ export default function Card() {
     const [title, updateTitle] = useState('');
     const [copy, updateCopy] = useState('');
     const [fade, setFade] = useState(false);
+    const [width, setWidth] = useState(0);
+    // const titleRef = useRef(null);
 
     useEffect(() => {
         updateTitle(data[0].title);
         updateCopy(data[0].copy);
         setTimeout(() => {
             setFade(true);
+            // if (titleRef.current) {
+            //     setWidth(titleRef.current.offsetWidth);
+            // }
         }, 50);
     }, []);
     function handleClick() {
@@ -23,11 +28,20 @@ export default function Card() {
         updateCopy(data[nextIndex].copy);
         setTimeout(() => {
             setFade(true);
+            // if (titleRef.current) {
+            //     setWidth(titleRef.current.offsetWidth);
+            // }
         }, 50);
     }
     return (
         <div className="info-card">
-            <h2 className={fade ? 'typing' : 'hidden'}>{data[count].title}</h2>
+            <h2
+                // ref={titleRef}
+                style={{ '--ch-length': `${data[count].titleLength}ch` }}
+                className={fade ? 'typing' : 'hidden'}
+            >
+                {data[count].title}
+            </h2>
             <p className={fade ? 'fade' : ''}>{data[count].copy}</p>
             <button onClick={handleClick}>Click for More Info</button>
         </div>
