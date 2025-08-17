1a83b87295dbeaf184547f088461a46941e0d235
George Arnold <george@groovecommerce.com>
2025-08-17 07:11:28 -0400

    added carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import selfPhoto from './assets/george-arnold-image.jpeg';
 import InfoCard from './InfoCard';
 import PoliticalChatBot from './PoliticalChatBot';
 import './App.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 function App() {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
+
     return (
         <>
             <div>
@@ -11,7 +33,10 @@ function App() {
             </div>
             <h1>George Arnold</h1>
             <p>Front-End Software Developer</p>
-            <PoliticalChatBot />
+            <Carousel responsive={responsive}>
+                <PoliticalChatBot />
+                <OldProjectList />
+            </Carousel>
             <InfoCard />
             <div className="card">
                 <a
---
+++ src/OldProjectList.jsx
@@ -0,0 +1,28 @@
+export function OldProjectList() {
+    return (
+        <>
+            <p>
+                This is the work I did before my first position as a
+                professional developer, much of the work is highly dated, but
+                showcases where I've come from
+            </p>
+            <ul>
+                <li>
+                    <a>My Old Portfolio</a>
+                </li>
+                <li>
+                    <a>My Old Github</a>
+                </li>
+                <li>
+                    <a>Character Sheet</a>
+                </li>
+                <li>
+                    <a>Budgeting App</a>
+                </li>
+                <li>
+                    <a>Get Out of Your bubble</a>
+                </li>
+            </ul>
+        </>
+    );
+}
